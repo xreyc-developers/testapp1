d09793790ca15e9bdcebc4d8b3b3abf1c95253de
xreyc <segumareyco@gmail.com>
2022-01-16 18:19:33 +0800

    cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(cors({
 require("./initialize/db")();
 require("./initialize/routes")(app);
 
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
